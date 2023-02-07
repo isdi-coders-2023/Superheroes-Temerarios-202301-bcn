51e852629eb2e49701fb765b93eea020c1bf3e45
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-02-07 21:00:37 +0100

    Fix context provider

--- src/store/contexts/recipes/RecipesContextProvider.tsx
+++ src/store/contexts/recipes/RecipesContextProvider.tsx
@@ -1,6 +1,7 @@
 import { useReducer } from "react";
 import recipesReducer from "../../reducers/recipesReducer";
 import RecipesContext from "./RecipesContext";
+import { useMemo } from "react";
 
 interface RecipesContextProviderProps {
   children: JSX.Element | JSX.Element[];
@@ -10,11 +11,10 @@ const RecipesContextProvider = ({
   children,
 }: RecipesContextProviderProps): JSX.Element => {
   const [recipes, dispatch] = useReducer(recipesReducer, []);
+  const foo = useMemo(() => ({ recipes, dispatch }), [recipes]);
 
   return (
-    <RecipesContext.Provider value={{ recipes, dispatch }}>
-      {children}
-    </RecipesContext.Provider>
+    <RecipesContext.Provider value={foo}>{children}</RecipesContext.Provider>
   );
 };
 
