b356829cfaf71fa57de55a04d44a9f600d6f2555
Sergi Casquero <sergicasquero@gmail.com>
2023-02-07 09:42:23 +0100

    Add Home component containing first fetch load

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./App.css";
+import { hitsStructure, linksStructure } from "./data/data";
+import Home from "./pages/home/home";
+
+interface responseStructure {
+  from: number;
+  to: number;
+  _links: linksStructure;
+  hits: hitsStructure;
+}
 
 function App() {
-  return <div className="App"></div>;
+  const baseUrl =
+    "https://api.edamam.com/api/recipes/v2?type=public&app_id=09cb5146&app_key=%20f097ebbc5080ade5ceb4e2f3a105d5b9&diet=balanced";
+
+  const recipeList = async (): Promise<responseStructure> => {
+    const list = await fetch(baseUrl);
+    const response = await list.json();
+    console.log(response);
+    return response;
+  };
+
+  recipeList();
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
