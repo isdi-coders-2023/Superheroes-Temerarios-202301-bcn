f1dda1d64e9d9d0f928d6f3b8352822a0ab72a99
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-02-08 19:02:56 +0100

    Add button component and it styles

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import Button from "../../components/Button/Button";
+import Loading from "../../components/Loading/Loading";
 
 const HomePage = (): JSX.Element => {
   return (
     <>
       <h1>Hack'n'Eat</h1>
-      <Button className="button-prova" content="" />
+      <Button className="button-prova" content="+" />
+      <Loading />
     </>
   );
 };
