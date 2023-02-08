733de6aa217f52e9959628dbfaefa34e0b2b6e9b
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-02-08 11:07:12 +0100

    Add home test and change home page component name

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
 import React from "react";
 import "./App.css";
-import { HitsStructure, LinksStructure } from "./data/data";
-import Home from "./pages/home/home";
-
-interface responseStructure {
-  from: number;
-  to: number;
-  _links: LinksStructure;
-  hits: HitsStructure;
-}
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
-  const baseUrl =
-    "https://api.edamam.com/api/recipes/v2?type=public&app_id=09cb5146&app_key=%20f097ebbc5080ade5ceb4e2f3a105d5b9&diet=balanced";
-
-  const recipeList = async (): Promise<responseStructure> => {
-    const list = await fetch(baseUrl);
-    const response = await list.json();
-    console.log(response);
-    return response;
-  };
-
-  recipeList();
   return (
     <div className="App">
-      <Home />
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/HomePage/HomePage.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import HomePage from "./HomePage";
+
+describe("Given a Home component", () => {
+  describe("When its rendered with a heading", () => {
+    test("Then it should show a heading on the screen", () => {
+      render(<HomePage />);
+
+      const heading = screen.getByRole("heading");
+
+      expect(heading).toBeInTheDocument();
+    });
+  });
+});
