dab3bd0bd68bbeacb6bf4b86dd9309ac748cbe8b
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-02-08 12:42:07 +0100

    Add AddButton tsx structure

---
+++ src/components/AddButton/AddButton.tsx
@@ -0,0 +1,5 @@
+const AddButton = (): JSX.Element => {
+  return <button>+</button>;
+};
+
+export default AddButton;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,12 @@
+import AddButton from "../../components/AddButton/AddButton";
+
 const HomePage = (): JSX.Element => {
-  return <h1>Hack'n'Eat</h1>;
+  return (
+    <>
+      <h1>Hack'n'Eat</h1>
+      <AddButton />
+    </>
+  );
 };
 
 export default HomePage;
