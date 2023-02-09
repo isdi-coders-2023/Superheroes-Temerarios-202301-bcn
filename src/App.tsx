35fea3ecde5b7ba4e2051db5ab12d38efc137be4
Sergi Casquero <sergicasquero@gmail.com>
2023-02-09 16:06:42 +0100

    Convert App component to arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import Error404Page from "./pages/Error404Page/Error404Page";
 import HomePage from "./pages/HomePage/HomePage";
 
-function App() {
+const App = () => {
   return (
     <div className="app">
       <Error404Page />
       <HomePage />
     </div>
   );
-}
+};
 
 export default App;
