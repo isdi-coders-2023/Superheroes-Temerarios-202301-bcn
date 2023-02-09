932b9abfcf417689d1b548bb9dc8decf520e84cb
Victor Granda <victorgranda1992@gmail.com>
2023-02-09 11:19:58 +0100

    Fix env variables to properly use on handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,13 +1,12 @@
 import { rest } from "msw";
 
 const apiUrl = process.env.REACT_APP_HACKANDEAT_URL!;
-const apiKey = "498953c8163cbec03246cd4914d809ec";
-
-const params = `field=label&field=image&imageSize=REGULAR&field=foodId&field=dishType&field=calories&field=cuisineType&field=totalNutrients&app_id=3ecd7851&type=public&app_key=${apiKey}`;
+const apiKey = process.env.REACT_APP_API_KEY!;
+const apiParams = process.env.REACT_APP_API_PARAMS!;
 
 export const handlers = [
-  rest.get(`${apiUrl}`, (req, res, ctx) => {
-    const queryParams = req.url.searchParams.getAll(params);
+  rest.get(apiUrl, (req, res, ctx) => {
+    const queryParams = req.url.searchParams.getAll(`${apiParams}${apiKey}`);
     return res(ctx.status(200), ctx.json(queryParams));
   }),
 ];
