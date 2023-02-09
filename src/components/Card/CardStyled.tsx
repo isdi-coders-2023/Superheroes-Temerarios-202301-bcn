8af6a1aae650e7631e6b3a6d1b6147bbffdb5f5c
Victor Granda <victorgranda1992@gmail.com>
2023-02-10 00:48:33 +0100

    Add CardStyled file

--- src/components/Card/CardStyled.tsx
+++ src/components/Card/CardStyled.tsx
@@ -2,57 +2,81 @@ import styled from "styled-components";
 
 export const CardStyled = styled.article`
   box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 320px;
   height: 300px;
-  background: #1f1d2b;
-  color: #ffffff;
-  font-size: 20px;
+  width: 320px;
+  border: 2px black solid;
   border-radius: 15px;
-  position: relative;
-  padding-top: 150px;
+  background-color: #1f1d2b;
+  position: absolute;
+  text-align: center;
+  color: #ffff;
 
-  &info {
+  .image {
+    height: 180px;
+    width: 190px;
+    border-radius: 50%;
+    position: relative;
+    top: -45px;
+  }
+
+  .info {
+    bottom: 55px;
+    padding-top: 10px;
+    position: relative;
     &__name {
       font-weight: 600;
       font-size: 24px;
       padding-bottom: 5px;
+      padding-top: 5px;
     }
     &__category {
       font-weight: 400;
       font-size: 20px;
-      margin-bottom: 45px;
+      margin-bottom: 25px;
     }
 
     &__stats {
       display: flex;
-      justify-content: space-between;
-      align-items: center;
-      gap: 35px;
+      justify-content: space-around;
+      font-size: 20px;
+      padding: 0 20px 0 20px;
+      gap: 130px;
+      padding-top: 20px;
+
       .fa-heart {
-        color: red;
+        box-sizing: border-box;
+        color: #ffff;
         position: absolute;
-        left: 145px;
-        bottom: 20px;
+        left: 137px;
+        bottom: -8px;
+        transition: color 0.5s;
+      }
+
+      .fa-heart:hover {
+        color: red;
+        font-size: 2.55rem;
       }
 
       .fa-clock {
       }
 
-      .stats__right {
-        color: #2edb73;
-        gap: 30px;
+      &_kcal {
+      }
+
+      &_minutes {
+        display: flex;
+        position: relative;
+        justify-content: space-between;
+        .minutes {
+          color: #2edb73;
+        }
+
+        .fa-clock {
+          position: absolute;
+          right: 60px;
+          align-self: center;
+        }
       }
     }
-  }
-
-  &__image {
-    max-width: 194px;
-    max-height: 187px;
-    border-radius: 50%;
-    top: -50px;
-    position: absolute;
   }
 `;
