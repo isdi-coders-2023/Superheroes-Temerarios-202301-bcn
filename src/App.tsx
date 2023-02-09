6d92c98e60d274a1003ec478defc145b5163dc6f
Sergi Casquero <sergicasquero@gmail.com>
2023-02-09 15:42:31 +0100

    Delete unused App.css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import "./App.css";
+import Error404Page from "./pages/Error404Page/Error404Page";
 import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
+      <Error404Page />
       <HomePage />
     </div>
   );
