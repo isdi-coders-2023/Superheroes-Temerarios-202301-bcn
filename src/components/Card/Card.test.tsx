513d2ab07362d5f12bd72e955669433d8b384bde
Victor Granda <victorgranda1992@gmail.com>
2023-02-10 21:33:36 +0100

    Fix card test and component

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,23 +1,58 @@
 import { render, screen } from "@testing-library/react";
+import { HitStructure } from "../../data/types";
 import Card from "./Card";
 
 describe("Given a Card component", () => {
   describe("When rendered", () => {
+    const fakeRecipe: HitStructure = {
+      recipe: {
+        label: "Patatetas",
+        image: "",
+        dietLabels: [],
+        healthLabels: [],
+        ingredients: [],
+        calories: 200,
+        cuisineType: [],
+        totalNutrients: {
+          ENERC_KCAL: { label: "", quantity: 50, unit: "" },
+          FAT: { label: "", quantity: 50, unit: "" },
+          CHOCDF: { label: "", quantity: 50, unit: "" },
+          FIBTG: { label: "", quantity: 50, unit: "" },
+          SUGAR: { label: "", quantity: 50, unit: "" },
+          PROCNT: { label: "", quantity: 50, unit: "" },
+          CHOLE: { label: "", quantity: 50, unit: "" },
+          VITA_RAE: { label: "", quantity: 50, unit: "" },
+          VITC: { label: "", quantity: 50, unit: "" },
+        },
+        totalDaily: {
+          ENERC_KCAL: { label: "", quantity: 1, unit: "" },
+          FAT: { label: "", quantity: 1, unit: "" },
+          CHOCDF: { label: "", quantity: 1, unit: "" },
+          FIBTG: { label: "", quantity: 1, unit: "" },
+          SUGAR: { label: "", quantity: 1, unit: "" },
+          PROCNT: { label: "", quantity: 1, unit: "" },
+          CHOLE: { label: "", quantity: 1, unit: "" },
+          VITA_RAE: { label: "", quantity: 1, unit: "" },
+          VITC: { label: "", quantity: 1, unit: "" },
+        },
+        totalTime: 50,
+      },
+    };
+
+    test("Then it should show an image", () => {
+      render(<Card recipe={fakeRecipe} />);
+      const imageRecipe = screen.getByRole("img");
+
+      expect(imageRecipe).toBeInTheDocument();
+    });
+
     test("Then it should show text with the name Patatetas", () => {
-      render(
-        <Card
-          category=""
-          image=""
-          name="Patatetas"
-          calories="500"
-          totalTime="20"
-        />
-      );
+      render(<Card recipe={fakeRecipe} />);
       const expectedText = "Patatetas";
 
-      const text = screen.getByText(expectedText);
+      const textResult = screen.getByText(expectedText);
 
-      expect(text).toBeInTheDocument();
+      expect(textResult).toBeInTheDocument();
     });
   });
 });
