792b07a45e0878b3fc73457dc807d37bdb4e4c30
Victor Granda <victorgranda1992@gmail.com>
2023-02-10 21:42:50 +0100

    Add useEffect and useContext to HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,30 @@
+import { useContext, useEffect } from "react";
 import Button from "../../components/Button/Button";
+import CardList from "../../components/CardList/CardList";
 import Loading from "../../components/Loading/Loading";
+import useApiRequest from "../../hooks/useGetRecipes";
+import RecipesContext from "../../store/contexts/recipes/RecipesContext";
 
 const HomePage = (): JSX.Element => {
+  const { getApiRecipes } = useApiRequest();
+
+  const {
+    recipes: { hits },
+  } = useContext(RecipesContext);
+
+  useEffect(() => {
+    getApiRecipes();
+  }, [getApiRecipes]);
+
   return (
     <>
       <Button
         aria-label="add-button"
         className="button-add"
-        content=<i className="fa-solid fa-plus"></i>
+        content={<i className="fa-solid fa-plus"></i>}
       />
-      <Loading />
+      {/* {hits.length ? <CardList recipes={hits} /> : <Loading />}
+       */}
     </>
   );
 };
