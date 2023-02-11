85a3dc9ef91b628b942034bff3dbb2df893bcec5
Sergi Casquero <sergicasquero@gmail.com>
2023-02-11 19:29:04 +0100

    Rename error404 main tag to h2

--- src/components/ErrorFourOFour/ErrorFourOFour.tsx
+++ src/components/ErrorFourOFour/ErrorFourOFour.tsx
@@ -3,7 +3,7 @@ import ErrorFourOFourStyled from "./ErrorFourOFourStyled";
 const ErrorFourOFour = (): JSX.Element => {
   return (
     <ErrorFourOFourStyled className="error404">
-      <span className="error404__title">Page not found</span>
+      <h2 className="error404__title">Page not found</h2>
       <span className="error404__number">404</span>
       <img
         className="error404__image"
